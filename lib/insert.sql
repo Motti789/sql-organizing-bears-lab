69009215dbc2871ae050bb97d5f05311d8a81b10
Motti789 <mchillywilly@gmail.com>
2021-11-04 12:15:11 -0400

    commit tree 0db105914badc01263eb6d46d497a0abe317138c

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ('Mr. Chocolate', 3, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Rowdy', 5, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Tabitha', 4, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Sergent Brown', 3, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Melissa', 3, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Grinch', 3, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Wendy', 3, 'F');
+INSERT INTO bears (name, age, sex) VALUES (NULL, 3, 'M');
